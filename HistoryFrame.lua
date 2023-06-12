8610074d5a56960f36ed64673a720ad27ee8868c
Ioney <dominik.hier@gmail.com>
2023-06-12 14:38:53 +0200

    some cleanup
    untested

--- HistoryFrame.lua
+++ HistoryFrame.lua
@@ -2,24 +2,26 @@ local ADDON_NAME, ns = ...
 
 local QuestHistory = {}
 
-function QuestHistory:Refresh()
+local function QuestList()
     local QuestList = {}
     local TIME = time()
     for id, quest in pairs(ns.QuestDB) do
         if quest.title then
             table.insert(QuestList, {
-                title = quest.title or UNKNOWN,
                 id = id,
-                time = quest.time and TIME - quest.time or TIME,
+                title = quest.title,
+                time = quest.time and TIME - quest.time or TIME, -- defaults to current time for sorting
                 map = quest.map and quest.map.name or UNKNOWN,
                 pos = {x = quest.x, y = quest.y}
             })
         end
     end
-    local dataProvider = CreateDataProvider(QuestList)
+    return QuestList
+end
 
+function QuestHistory:Refresh()
+    local dataProvider = CreateDataProvider(QuestList())
     dataProvider:SetSortComparator(function(A, B) return A.time < B.time end)
-
     self.Frame.QuestListContainer.ScrollView:SetDataProvider(dataProvider)
 end
 
@@ -30,45 +32,29 @@ function QuestHistory:Init()
 
     local initializer = function(line, quest)
         line:SetHighlightAtlas("auctionhouse-ui-row-highlight")
-        if not line.Title then
-            line.Id = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightRight")
-            line.Id:SetPoint("LEFT", 10, 0)
 
-            line.Title = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightLeft")
-            line.Title:SetPoint("LEFT", 100, 0)
-
-            line.Map = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightLeft")
-            line.Map:SetPoint("RIGHT", -200, 0)
-
-            line.Position = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightLeft")
-            line.Position:SetPoint("RIGHT", -100, 0)
-
-            line.Time = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightRight")
-            line.Time:SetPoint("RIGHT")
-        end
-
+        line.Id = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightRight")
+        line.Id:SetPoint("LEFT", 10, 0)
         line.Id:SetText(quest.id)
-        line.Title:SetText(quest.title or UNKNOWN)
+
+        line.Title = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightLeft")
+        line.Title:SetPoint("LEFT", 100, 0)
+        line.Title:SetText(quest.title)
+
+        line.Map = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightLeft")
+        line.Map:SetPoint("RIGHT", -200, 0)
         line.Map:SetText(quest.map and quest.map.name or UNKNOWN)
+
+        line.Position = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightLeft")
+        line.Position:SetPoint("RIGHT", -100, 0)
         line.Position:SetText(format("%.2f %.2f", quest.pos.x * 100, quest.pos.y * 100))
+
+        line.Time = line:CreateFontString(nil, "ARTWORK", "GameFontHighlightRight")
+        line.Time:SetPoint("RIGHT")
         line.Time:SetText(ns.QuestDB[quest.id].time and SecondsToTime(quest.time, true) or UNKNOWN)
     end
 
-    local QuestList = {}
-    local TIME = time()
-    for id, quest in pairs(ns.QuestDB) do
-        if quest.title then
-            table.insert(QuestList, {
-                title = quest.title or UNKNOWN,
-                id = id,
-                time = quest.time and TIME - quest.time or TIME,
-                map = quest.map and quest.map.name or UNKNOWN,
-                pos = {x = quest.x, y = quest.y}
-            })
-        end
-    end
-
-    local dataProvider = CreateDataProvider(QuestList)
+    local dataProvider = CreateDataProvider(QuestList())
 
     dataProvider:SetSortComparator(function(A, B) return A.time < B.time end)
 
