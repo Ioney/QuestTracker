31bda8898c86c0933b4b8457d65a273123794477
Ioney <dominik.hier@gmail.com>
2023-06-22 15:40:35 +0200

    Fixed ForcedNodes.lua

--- Zar_ForceNodes.lua
+++ Zar_ForceNodes.lua
@@ -1,18 +1,16 @@
 local ADDON_NAME, ns = ...
 
-local plugins = 'HandyNotes_ZarPlugins'
-if _G[plugins] == nil then _G[plugins] = {} end
-_G[plugins][#_G[plugins] + 1] = ns
+local groupPins = WorldMapFrame.pinPools.GroupMembersPinTemplate
 
 -- Listen for LCTRL + LALT when the map is open to force display nodes
 local IQFrame = CreateFrame('Frame', ADDON_NAME .. 'IQ', WorldMapFrame)
-local groupPins = WorldMapFrame.pinPools.GroupMembersPinTemplate
 IQFrame:SetPropagateKeyboardInput(true)
+
 IQFrame:SetScript('OnKeyDown', function(_, key)
     if (key == 'LCTRL' or key == 'LALT') and IsLeftControlKeyDown() and IsLeftAltKeyDown() then
         IQFrame:SetPropagateKeyboardInput(false)
-        for i, _ns in ipairs(_G[plugins]) do
-            if _ns and _ns.addon and not _ns.dev_force then
+        for i, _ns in ipairs(_G['HandyNotes_ZarPlugins']) do
+            if _ns and _ns.addon then
                 _ns.dev_force = true
                 _ns.addon:RefreshImmediate()
             end
@@ -21,11 +19,12 @@ IQFrame:SetScript('OnKeyDown', function(_, key)
         groupPins:GetNextActive():Hide()
     end
 end)
+
 IQFrame:SetScript('OnKeyUp', function(_, key)
     if key == 'LCTRL' or key == 'LALT' then
         IQFrame:SetPropagateKeyboardInput(true)
-        for i, _ns in ipairs(_G[plugins]) do
-            if _ns and _ns.addon and _ns.dev_force then
+        for i, _ns in ipairs(_G['HandyNotes_ZarPlugins']) do
+            if _ns and _ns.addon then
                 _ns.dev_force = false
                 _ns.addon:RefreshImmediate()
             end
@@ -34,3 +33,4 @@ IQFrame:SetScript('OnKeyUp', function(_, key)
         groupPins:GetNextActive():Show()
     end
 end)
+
